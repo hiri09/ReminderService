43ca3f8e06198ae3af2b8a749b679dff13fc54bc
himanshu kumar <hk7703940916@gmail.com>
2023-07-01 17:27:35 +0530

    Setup of smpt sever

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 
 const {PORT} = require('./config/serverConfig');
 const app = express();
+const {sendBasicEmail} = require('./services/email-service');
 
 const setupAndStartServer = ()=>{
    
@@ -10,6 +11,13 @@ const setupAndStartServer = ()=>{
     app.use(bodyParser.urlencoded({extended : true}));
     app.listen(PORT , ()=>{
         console.log(`sever is running at ${PORT}`);
+
+        sendBasicEmail(
+            'support@admin.com',
+            'hk8810254@gmail.com',
+            'This is testing email',
+            'Hi , How are you'
+        )
     })
 }
 setupAndStartServer();
