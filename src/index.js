768f5e98a7d0b33f96437564685735b06b42cf36
himanshu kumar <hk7703940916@gmail.com>
2023-07-01 16:36:57 +0530

    Setup the basic server

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const {PORT} = require('./config/serverConfig');
+const app = express();
+
+const setupAndStartServer = ()=>{
+   
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended : true}));
+    app.listen(PORT , ()=>{
+        console.log(`sever is running at ${PORT}`);
+    })
+}
+setupAndStartServer();
